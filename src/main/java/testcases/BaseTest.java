c5fe394a44843471b317d932747ad032ae7aabd7
neerajsdet <neerajagarwal090@gmail.com>
2023-12-03 12:11:22 +0530

    updating chrome browser name

--- src/main/java/testcases/BaseTest.java
+++ src/main/java/testcases/BaseTest.java
@@ -25,7 +25,7 @@ public class BaseTest {
 
     @BeforeClass
     public void setUp(){
-        driver = BrowserFactory.launchGivenBrowser("firefox");
+        driver = BrowserFactory.launchGivenBrowser("chrome");
         homePage = new HomePage(driver);
         productDetailsPage = new ProductDetailsPage(driver);
         driver.get("https://magento.softwaretestingboard.com/");
